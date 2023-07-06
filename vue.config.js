5ff6ce5e14004b54e4565bee7d9cd96c79a9ca36
KONSTANTYN <konstantyn.kravchenko.work@gmai.com>
2023-07-06 14:53:09 +0300

    ready to start

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/design/_colors.scss";
+          @import "@/design/_typography.scss";
+        `,
+      },
+    },
+  },
 });
